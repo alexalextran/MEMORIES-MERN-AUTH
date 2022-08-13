b905ff5eea27059426f89f55f16d38aeceac5486
Alex Tran <alextran8385@gmail.com>
2022-08-14 00:19:04 +1000

    sign in component

---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { TextField, Grid, InputAdornment, Icon} from '@material-ui/core';
+const Input = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Input;
