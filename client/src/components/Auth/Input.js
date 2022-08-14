1c0df104f9066bd4cc007457e2a5627e3f079777
Alex Tran <alextran8385@gmail.com>
2022-08-14 13:46:53 +1000

    input components for auth

--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { TextField, Grid, InputAdornment, Icon} from '@material-ui/core';
-const Input = () => {
+import { TextField, Grid, InputAdornment, IconButton} from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
+const Input = ({name, half, handleChange, label, autoFocus, handleShowPassword, type}) => {
     return (
-        <div>
-            
-        </div>
+        <Grid item xs={12} sm={half ? 6 : 12}>
+            <TextField name={name} onChange={handleChange} variant="outlined" required fullWidth label={label} autoFocus={autoFocus} type={type} inputProps={name === 'password' && {
+                endAdornment: (
+                <InputAdornment position="end">
+                <IconButton onClick={handleShowPassword}>
+                {type === "password" ? <Visibility/> : <VisibilityOff />}
+                </IconButton>
+                </InputAdornment>
+                )
+            }}/>
+        </Grid>
     );
 }
 
