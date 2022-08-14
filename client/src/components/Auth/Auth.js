bd6e8769561244508b76abfb8fe932eaadf618f9
Alex Tran <alextran8385@gmail.com>
2022-08-14 18:58:03 +1000

    user profile

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,78 +1,99 @@
 import React, { useState } from 'react';
-import {Avatar, Button, Paper, Grid, Typography, Container, Icon} from '@material-ui/core'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import useStyles from './styles'
-import Input from './Input';
-import {GoogleLogin} from '@react-oauth/google'
 import { useDispatch } from 'react-redux';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import { GoogleLogin } from '@react-oauth/google';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { useNavigate } from 'react-router-dom';
+import Icon from './icon';
+import jwt_decode from 'jwt-decode'
 
+import { AUTH } from '../../constants/actionTypes';
+import useStyles from './styles';
+import Input from './Input';
 
-const Auth = () => {
-    const classes = useStyles()
-    const [showPassword, setshowPassword] = useState(false);
-    const [isSignup, setisSignup] = useState(false);
-    const handleSubmit = () => {
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
 
+const SignUp = () => {
+  let navigate = useNavigate();
+  const [form, setForm] = useState(initialState);
+  const [isSignup, setIsSignup] = useState(false);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = () => setShowPassword(!showPassword);
+
+  const switchMode = () => {
+    setForm(initialState);
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    setShowPassword(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+   
+  };
+
+  const googleSuccess = async (res) => {
+  
+    var data = jwt_decode(res.credential)
+    try {
+      dispatch({ type: AUTH, data: { data }});
+
+      navigate('/');
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleShowPassword = () => {
-        setshowPassword(!showPassword)
-    }
+  const googleError = () => alert('Google Sign In was unsuccessful. Try again later');
 
-    const handleChange = () => {
-    }
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
-    const googleSuccess = async (res) => {
-        console.log(res)
-    }
-    const googleFailure = (error) => {
-        console.log(error)
-        console.log("sign in was unsuccsessful")
-    }
-
-    
-    return (
-        <Container component="main" maxWidth="xs">
-        <Paper className={classes.paper} elevation={3}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">{ isSignup ? 'Sign up' : 'Sign in' }</Typography>
-          <form className={classes.form} onSubmit={handleSubmit}>
-            <Grid container spacing={2}>
-              { isSignup && (
-              <>
-                <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
-                <Input name="lastName" label="Last Name" handleChange={handleChange} half />
-              </>
-              )}
-              <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
-              <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
-              { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
-            </Grid>
-            <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
-              { isSignup ? 'Sign Up' : 'Sign In' }
-            </Button>
-            <GoogleLogin render={(renderProps) => (
-              <Button className={classes.googleButton} color="primary" fullWidth={true} onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">{ isSignup ? 'Sign up' : 'Sign in' }</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            { isSignup && (
+            <>
+              <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+              <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+            </>
+            )}
+            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+            <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+            { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+            { isSignup ? 'Sign Up' : 'Sign In' }
+          </Button>
+          <GoogleLogin
+            render={(renderProps) => (
+              <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
                 Google Sign In
               </Button>
             )}
             onSuccess={googleSuccess}
-            onFailure={googleFailure}
-            cookiePolicy="single_host_origin"/>
-            <Grid container justifyContent="flex-end" >
-                <Grid item>
-                    <Button onClick={() => {setisSignup(!isSignup)}}>{
-                        isSignup ? 'Already have an account? Sign In' : "Don't have an account? Sign Up"
-                    }</Button>
-                </Grid>
-
+            onFailure={googleError}
+            cookiePolicy="single_host_origin"
+          />
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                { isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
+              </Button>
             </Grid>
-                </form>
-            </Paper>
-        </Container>
-    );
-}
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
 
-export default Auth;
+export default SignUp;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppBar, Avatar, Toolbar, Typography, Button} from '@material-ui/core'
 import memories from '../../Images/memories.png'
 import useStyles from './styles'
@@ -7,27 +7,30 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
     const classes = useStyles()
 
-    const user = null;
-    return (
-        <AppBar className={classes.appBar} position="static" color="inherit">
-            <div className={classes.brandContainer}>
-            <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
-                    <img className={classes.image} src={memories} alt="icon" height="60" />
-            </div>
-        <Toolbar className={classes.toolbar}>
-            {user ? (
-            <div className={classes.profile}>
-                <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                <Button variant="contained" classname={classes.logout} color="secondary">Logout</Button>
-            </div>
-            ) 
-            : (
-                <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
-            )}
-        </Toolbar>
-      </AppBar>
-    );
-}
+const [user, setuser] = useState(JSON.parse(localStorage.getItem('profile')).data)
+
+console.log(user) 
+
+return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user?.name} src={user?.imageUrl}>{user?.name.charAt(0)}</Avatar>
+            <Typography className={classes.userName} variant="h6">{user?.name}</Typography>
+            <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+          </div>
+        ) : (
+          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+ 
 
 export default Navbar;
